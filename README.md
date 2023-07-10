3027753430c3a1d1ba8d84dc04ba459bad64e242
Nathalia Molina Da Silva <nm018692@gmail.com>
2023-07-10 15:57:16 -0300

    .

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # projeto-senac
+criar a pagina principal do site index
+colocar as fotos do portifolio e logo
+adicionar o bodstrap 
+arumar a conexão do php 
+
  
---
+++ includes/cabecalho.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <!-- bootstrap css-->
+    <!-- bootstrap icons -->
+    <!-- meu estilo.css -->
+</head>
+
+<body style="background: black; color:white; font-family: Arial;">
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">  
+                <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+                    <div class="navbar-nav ">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" href="#">Portfólio</a>
+                        <a class="nav-link" href="#">Quem Somos</a>
+                        <a class="nav-link" href="#">Materiais</a>
+                        <a class="nav-link" href="#">Biosegurança</a>
+                        <a class="nav-link" href="#">Contato</a>
+                    </div>
+                </div>
+            </div> 
+        </nav>
+        <center>
+            <a href="../index.php">
+                <img style="width: 80px;; padding: 5px;" src="../image/logo.png" width="150" >
+            </a>
+        </center>
+
+    </header>
+</body>
+
+</html>

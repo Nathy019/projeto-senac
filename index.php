8c804fa07a0bce8ee89485529b45d27a39ab18d4
Nathalia Molina Da Silva <nm018692@gmail.com>
2023-07-12 19:54:45 -0300

    .

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 include "./includes/cabecalho.php"
 ?>
+<!-- inicio carossel -->
 <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-    <!-- inicio carossel -->
+
     <div class="carousel-inner mx-auto p-2" style=" height:40rem;  width:50rem; opacity:0.4;">
         <div class="carousel-item active ">
             <img src="imagem/tatu1.jpg" class="d-block w-100" alt="...">
@@ -22,7 +23,7 @@ include "./includes/cabecalho.php"
         <div class="carousel-item">
             <img src="imagem/tatucop6.jpg" class="d-block w-100" alt="...">
         </div>
-    </div> 
+    </div>
     <!-- inicio indicadores -->
     <div class="carousel-indicators">
         <button type="button" data-bs-target="carouselExampleAutoplaying" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -32,26 +33,42 @@ include "./includes/cabecalho.php"
         <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="4" aria-label="Slide 5"></button>
         <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="5" aria-label="Slide 6"></button>
     </div>
-    <!-- fim carossel -->
+    <!-- fim indicadores -->
 </div>
-<div class="card m-5 p-2" style="max-width: 540rem;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="imagem/fotodani.jpg" class="img-fluid rounded-start" alt="...">
+<!-- fim carossel -->
+
+<!-- Começo da parte da historia dele -->
+<div class="card text-bg-secondary bg-opacity-10 m-5 p-2" style="max-width: 540rem;">
+    <div class="row g-0">
+        <div class="col-md-4 ">
+            <img src="imagem/fotodani.jpg" class="img-fluid rounded-start" alt="foto do daniel">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title text-center p-5">Prazer sou o Daniel</h5>
+                <p class="card-text lh-lg text-lg-start">Acabei entrando na tattoo por conta da época de escola,
+                    que desenhava em todo mundo de canetinha
+                    e tudo que precisava de desenho sempre ficou
+                    por minha conta, até que quando pude procurei
+                    alguns amigos e tatuadores pra me ajudarem
+                    a começar na área, hoje fazem 2 anos que tatuo,
+                    comecei por hobby e consequentemente
+                    hoje é de onde vem minha maior renda.</p>
+            </div>
+        </div>
     </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Prazer sou o Daniel</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-      </div>
+</div>
+<!-- Fim da parte da historia dele -->
+
+<!-- começo do portifolio -->
+<div class="card text-bg-secondary bg-opacity-10 m-5 p-2" style="width: 18rem;">
+    <img src="imagem/tatu1.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title text-center">Primeira foto</h5>
+        <a href="portifolio/foto1.php" class="btn btn-primary">VER DESCRIÇÃO</a>
     </div>
-  </div>
 </div>
 <!--
-
-colocar um card com a foto dele e a descrição do pq ele começou a tatutar em baixo o insta dele para seguir a pagina dele
-
 fazendo cards tematicos com o portifolio dele e colocando um botão e na hora que clicar aparecer uma aba de notifição com a legenda da foto
 
 colocar dois cards com a bio e esquipamentos que ele utiliza e colocando uma legenda

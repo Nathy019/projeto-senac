05b00130befc34231f7fea3551978622ba685028
Nathalia Molina Da Silva <nm018692@gmail.com>
2023-07-14 14:22:13 -0300

    .

--- includes/modal.php
+++ includes/modal.php
@@ -7,7 +7,9 @@
         <img src="imagem/tatu1.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title text-center">Primeira foto</h5>
-            <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal0">VER DETALHES</button>
+            <div class="d-grid gap-1 mx-auto">
+                <button type="button" class="btn btn-outline-secondary " data-bs-toggle="modal" data-bs-target="#JanelaModal0">VER DETALHES</button>
+            </div>
         </div>
     </div>
 
@@ -15,14 +17,18 @@
         <img src="imagem/tatu2.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title text-center">Segunda foto</h5>
-            <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal1">VER DETALHES</button>
+            <div class="d-grid gap-1 mx-auto">
+                <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal1">VER DETALHES</button>
+            </div>
         </div>
     </div>
     <div class="card text-bg-secondary bg-opacity-10 m-5 p-2" style="width: 18rem;">
         <img src="imagem/tatu3.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title text-center">Terceira foto</h5>
-            <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal2">VER DETALHES</button>
+            <div class="d-grid gap-1 mx-auto">
+                <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal2">VER DETALHES</button>
+            </div>
         </div>
     </div>
 
@@ -30,7 +36,9 @@
         <img src="imagem/tatu4.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title text-center">Quarta foto</h5>
-            <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal3">VER DETALHES</button>
+            <div class="d-grid gap-1 mx-auto">
+                <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal3">VER DETALHES</button>
+            </div>
         </div>
     </div>
 </div>
@@ -40,7 +48,9 @@
         <img src="imagem/tatu5.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title text-center">Quinta foto</h5>
-            <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal4">VER DETALHES</button>
+            <div class="d-grid gap-1 mx-auto">
+                <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal4">VER DETALHES</button>
+            </div>
         </div>
     </div>
 
@@ -48,21 +58,27 @@
         <img src="imagem/tatucop6.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title text-center">Sexta foto</h5>
-            <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal5">VER DETALHES</button>
+            <div class="d-grid gap-1 mx-auto">
+                <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal5">VER DETALHES</button>
+            </div>
         </div>
     </div>
     <div class="card text-bg-secondary bg-opacity-10 m-5 p-2" style="width: 18rem;">
         <img src="imagem/tatu7.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title text-center">Setima foto</h5>
-            <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal6">VER DETALHES</button>
+            <div class="d-grid gap-1 mx-auto">
+                <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal6">VER DETALHES</button>
+            </div>
         </div>
     </div>
     <div class="card text-bg-secondary bg-opacity-10 m-5 p-2" style="width: 18rem;">
         <img src="imagem/tatu8.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title text-center">Oitava foto</h5>
-            <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal7">VER DETALHES</button>
+            <div class="d-grid gap-1 mx-auto">
+                <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal7">VER DETALHES</button>
+            </div>
         </div>
     </div>
 </div>
@@ -71,35 +87,45 @@
         <img src="imagem/tatu9.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title text-center p-3">Nona foto</h5>
-            <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal8">VER DETALHES</button>
+            <div class="d-grid gap-1 mx-auto">
+                <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal8">VER DETALHES</button>
+            </div>
         </div>
     </div>
     <div class="card text-bg-secondary bg-opacity-10 m-5 p-2" style="width: 18rem;">
         <img src="imagem/tatu10.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title text-center p-3">Decima foto</h5>
-            <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal9">VER DETALHES</button>
+            <div class="d-grid gap-1 mx-auto">
+                <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal9">VER DETALHES</button>
+            </div>
         </div>
     </div>
     <div class="card text-bg-secondary bg-opacity-10 m-5 p-2" style="width: 18rem;">
         <img src="imagem/tatucop11.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title text-center">Decima primeria foto</h5>
-            <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal10">VER DETALHES</button>
+            <div class="d-grid gap-1 mx-auto">
+                <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal10">VER DETALHES</button>
+            </div>
         </div>
     </div>
     <div class="card text-bg-secondary bg-opacity-10 m-5 p-2" style="width: 18rem;">
         <img src="imagem/tatucop12.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title text-center">Decima Segunda foto</h5>
-            <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal11">VER DETALHES</button>
+            <div class="d-grid gap-1 mx-auto">
+                <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal11">VER DETALHES</button>
+            </div>
         </div>
     </div>
     <div class="card text-bg-secondary bg-opacity-10 m-5 p-2" style="width: 18rem;">
         <img src="imagem/tatucop13.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title text-center">Decima Terceira foto</h5>
-            <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal12">VER DETALHES</button>
+            <div class="d-grid gap-1 mx-auto">
+                <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#JanelaModal12">VER DETALHES</button>
+            </div>
         </div>
     </div>
 </div>
